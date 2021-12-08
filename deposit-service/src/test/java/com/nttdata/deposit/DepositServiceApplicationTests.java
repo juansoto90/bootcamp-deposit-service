454e2aea9ee91c53c8a16a913d0d9eeff92e2094
Juan Soto <jsotcast@nttdata.com>
2021-12-07 20:58:27 -0500

    refactor add bootstrap configuration

---
+++ deposit-service/src/test/java/com/nttdata/deposit/DepositServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.deposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepositServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
